9cd87540481bf54ec260e7c932a9eb2ec3049672
andrewnetgain <andrewm@netgainseo.com>
2019-09-30 15:41:54 -0400

    privacy policy

--- footer.php
+++ footer.php
@@ -2,9 +2,13 @@
 
 <footer class="container-fluid site-footer">
     <div class="container flex-footer">
-        <div class="f-item copyright">Copyright &copy; <?php echo date("Y"); ?> Canada’s Top Mayor Award. All rights reserved.</div>
+        <div class="f-item copyright">Copyright &copy; <?php echo date("Y"); ?> Canada’s Top Mayor Award. All rights reserved. 
+        	<p style="font-size: 16px">View our <a style="color: #fff;font-size: 16px" href="https://canadastopmayoraward.com/privacy-policy/">Privacy Policy</a></p>
+        </div>
 
-        <div class="f-item netgain">Website Designed by <a href="http://www.netgainseo.com/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/netgain.png" alt=""></a></div>
+        <div class="f-item netgain">Website Designed by <a href="http://www.netgainseo.com/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/netgain.png" alt=""></a>
+
+        </div>
     </div>
 </footer>
 <a href="#" class="scrollToTop">&uarr;</a>
